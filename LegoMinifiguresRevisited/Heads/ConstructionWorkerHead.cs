20aa0b683f474b407f81c44a57735df20631b607
BenJarrett <b.cl.jarrett@gmail.com>
2021-07-30 18:43:48 -0500

    Created a class called 'ConstructionWorkerHeads' Declared it's properties, and wrote a few methods that contain behaviors

--- LegoMinifiguresRevisited/Heads/ConstructionWorkerHead.cs
+++ LegoMinifiguresRevisited/Heads/ConstructionWorkerHead.cs
@@ -8,6 +8,37 @@ namespace LegoMinifiguresRevisited.Heads
 {
     class ConstructionWorkerHead
     {
-        
+        public bool HasMullet { get; set; }
+        public string Color { get; set; }
+        public int NumberOfTeeth { get; set; }
+        public bool WearingHardHat { get; set; }
+
+        // This is still a behavior. It does not always have to be void and return nothing //
+        // We are setting the property 'WearingHardHat' to the opposite value //
+        // We then return the value of the result of the 'WearingHardHat' property //
+        // Using a Ternary to write whether the worker is wearing or not wearing their hard hat // 
+        public bool ToggleWorking()
+        {
+            WearingHardHat = !WearingHardHat;
+
+            Console.WriteLine($"The Construction Worker Head {(WearingHardHat ? "is" : "is not")} wearing their hard hat.");
+
+            return WearingHardHat;
+        }
+
+        // Every time this method is initiated, the worker will lose 1 tooth // 
+        // The iff block will not run if the worker does not have more than 0 teeth
+        public void Spin()
+        {
+            if (NumberOfTeeth > 0) ;
+            NumberOfTeeth -= 1;
+
+            Console.WriteLine($"The {Color} Construction Worker Head spins after being hit by a girder.");
+        }
+
+        public void SayHi()
+        {
+            Console.WriteLine("The Construction Worker Head says hello.");
+        }
     }
 }

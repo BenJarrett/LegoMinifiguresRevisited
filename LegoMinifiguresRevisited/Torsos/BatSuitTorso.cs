53f2e71a0d5ae556b8e2f2e795ef78b9f47c7b6d
BenJarrett <b.cl.jarrett@gmail.com>
2021-08-02 12:35:33 -0500

    Created a BatsuitTorso Class. Definded it's properties and methods. This class inherits from the TorseBase class.

---
+++ LegoMinifiguresRevisited/Torsos/BatSuitTorso.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegoMinifiguresRevisited.Torsos
+{
+    class BatSuitTorso : TorsoBase
+    {
+        public bool HasBatBoomerang { get; set; }
+
+
+        public override void Greeting()
+        {
+            base.Greeting();
+            Console.WriteLine("Gives fist bump.");
+        }
+
+        public override void Wiggle()
+        {
+            Console.WriteLine("Flex on em!");
+        }
+
+        public void Donates()
+        {
+            Console.WriteLine("Donates 150 million to Children Are The Future Charity.");
+        }
+    }
+}
